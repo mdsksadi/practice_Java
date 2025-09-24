7e3d953b2207ff8e6149d43a729962157e683837
sadi <sadiewu12345@gmail.com>
2025-09-24 19:10:10 +0200

    Assignment_1 added
    - It is solved without using any variable

---
+++ Assignment_1.java
@@ -0,0 +1,24 @@
+/**
+ * Assignment 1
+ * @author Shekh Sadi    
+ *  Create a class called Product
+ *	- Create a main method
+ *	    -Print the following data in main method 
+ *			-Id: 101, 
+ *			-Title: iphone15, 
+ *			-Price: 1895€, 
+ *			-Description: Perfect produch with best image quality, 
+ *			-Category: Pthone 
+ */
+
+ public class Product
+ {
+    public static void main(String[] args)
+    {
+        System.out.println("Id: 101");
+        System.out.println("Title: iphone15");
+        System.out.println("Price: 1895€");
+        System.out.println("Description: Perfect produch with best image quality");
+        System.out.println("Category: Pthone");
+    }
+ }
