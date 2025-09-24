afc7dedfa1d04df3c80c2f5ccc1e2a6a8941f0ca
sadi <sadiewu12345@gmail.com>
2025-09-24 19:15:15 +0200

    DataType.java added
    - Here I wrote all the possible data type and handle policy

---
+++ DataType.java
@@ -0,0 +1,38 @@
+/**
+ * Java Program to demonstrate the use of different data types
+ * @author Shekh Sadi   
+ * Date: 24-09-2025
+ */
+
+public class DataType
+{
+    public static void main(String[] args)
+    {
+        boolean b;
+        char c;
+        short s= 25; // Dynamic initialization
+        int i;
+        float f;
+        double d;
+        
+        b = false;
+        System.out.println("b= "+b);
+        
+        c = 'a';
+        System.out.println("c= "+c);
+        
+        System.out.println("s= "+s);
+        
+        i = 4644;
+        System.out.println("i= "+i);
+        
+        f = 3.14f;
+        System.out.println("f= "+f);
+        
+        d = 3.141654545;
+        System.out.println("d= "+d);
+        
+        long L = 54584515414366L;
+        System.out.println("L= "+L);
+    }
+}
